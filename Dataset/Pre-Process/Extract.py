4d3c82a9959f1832b4503025e4be405d0624d14e
raymond040 <julianto.raymondfendy@gmail.com>
2022-10-21 21:33:48 +0800

    Update Extract.py

--- Dataset/Pre-Process/Extract.py
+++ Dataset/Pre-Process/Extract.py
@@ -2,16 +2,17 @@ import scipy.io
 import pandas as pd
 mat = scipy.io.loadmat('/workspaces/TCGConv/Dataset/YelpCHI/YelpChi.mat') 
 
-net_rur = pd.DataFrame(mat['net_rur']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/net_rur.csv')
-net_rtr = pd.DataFrame(mat['net_rtr']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/net_rtr.csv')
-net_rsr = pd.DataFrame(mat['net_rsr']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/net_rsr.csv')
-features = pd.DataFrame(mat['features']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/features.csv')
-homo = pd.DataFrame(mat['homo']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/homo.csv')
-label = pd.DataFrame(mat['label']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/label.csv')
+# Dont Run locally, will crash
+# net_rur = pd.DataFrame(mat['net_rur']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/net_rur.csv')
+# net_rtr = pd.DataFrame(mat['net_rtr']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/net_rtr.csv')
+# net_rsr = pd.DataFrame(mat['net_rsr']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/net_rsr.csv')
+# features = pd.DataFrame(mat['features']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/features.csv')
+# homo = pd.DataFrame(mat['homo']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/homo.csv')
+# label = pd.DataFrame(mat['label']).to_csv('/workspaces/TCGConv/Dataset/YelpCHI/label.csv')
 
-label
-net_rur
-net_rtr
-net_rsr
-features
-homo
+# label
+# net_rur
+# net_rtr
+# net_rsr
+# features
+# homo
