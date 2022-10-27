f078d84f503e409b18ca329da93fd8359360e75f
Raymond Fendy Julianto <e0407728@atlas9-c01.hpc.local>
2022-10-27 17:35:43 +0800

    update shells script

--- Shell/W_TCG.sh
+++ Shell/W_TCG.sh
@@ -10,7 +10,7 @@ cd #PBS_o_WORKDIR;
 np=$(cat ${PBS_NODEFILE} | wc -l);
 
 image="/home/svu/e0407728/SIF/edge-hpc_v0.1.sif"
-singularity exec -e $image bash << EOF > stdout.$PBS_JOBID 2> stderr.$PBS_JOBID 
+singularity exec -e $image bash << EOF > $PBS_JOBID.out.$PBS_JOBNAME 2> $PBS_JOBID.err.$PBS_JOBNAME 
 
 python3 "/home/svu/e0407728/My_FYP/TCGConv/main.py" --dataset_name "W" --percentage 1 --type_ED "sub" --num_groups 30 --root "/home/svu/e0407728/My_FYP/TCGConv/" --num_version $PBS_JOBID --seed $PBS_JOBID --alpha 0.01935191113 --gamma 3 --lr 4.19e-5 --weight_decay 4.47e-6 --hidden_chnl 128 --dropout 0.5 --num_layers 2 --model_type "TCGConv"
 EOF
