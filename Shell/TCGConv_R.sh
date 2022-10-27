d32d207f156429064cc3425d8a6ebe5dd781019c
raymond040 <julianto.raymondfendy@gmail.com>
2022-10-27 17:35:18 +0800

    update

--- Shell/TCGConv_R.sh
+++ Shell/TCGConv_R.sh
@@ -12,5 +12,5 @@ np=$(cat ${PBS_NODEFILE} | wc -l);
 image="/home/svu/e0407728/SIF/edge-hpc_v0.1.sif"
 singularity exec -e $image bash << EOF > $PBS_JOBID.out 2> $PBS_JOBID.err 
 
-python3 "/home/svu/e0407728/My_FYP/TCGConv/main.py" --dataset_name "R" --percentage 1 --type_ED "sub" --num_groups 60 --root "/home/svu/e0407728/My_FYP/TCGConv/" --num_version $PBS_JOBID --seed $PBS_JOBID --alpha 0.01935191113 --gamma 3 --lr 4.19e-5 --weight_decay 4.47e-6 --hidden_chnl 128 --dropout 0.5 --num_layers 2 --model_type "TCGConv"
+python3 "/home/svu/e0407728/My_FYP/TCGConv/main.py" --dataset_name "R" --percentage 1 --type_ED "sub" --num_groups 30 --root "/home/svu/e0407728/My_FYP/TCGConv/" --num_version $PBS_JOBID --seed $PBS_JOBID --alpha 0.01935191113 --gamma 3 --lr 4.19e-5 --weight_decay 4.47e-6 --hidden_chnl 128 --dropout 0.5 --num_layers 2 --model_type "TCGConv"
 EOF
