f9ff4ec2570fc4076a671d1487a7949ff45bd078
Raymond Fendy Julianto <e0407728@atlas9-c01.hpc.local>
2022-12-17 11:24:27 +0800

    shell files

--- Shell/R_TCG.sh
+++ Shell/R_TCG.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #PBS -P Project
 #PBS -j oe
-#PBS -N out
 #PBS -q volta_gpu
 #PBS -l select=1:ncpus=8:mem=80gb:ngpus=1
 #PBS -l walltime=12:00:00
@@ -10,7 +9,7 @@ cd #PBS_o_WORKDIR;
 np=$(cat ${PBS_NODEFILE} | wc -l);
 
 image="/home/svu/e0407728/SIF/edge-hpc_v0.1.sif"
-singularity exec -e $image bash << EOF > $PBS_JOBID.out 2> $PBS_JOBID.err 
+singularity exec -e $image bash << EOF > $PBS_JOBID.$PBS_JOBNAME.out 2> $PBS_JOBID.$PBS_JOBNAME.err
 
 python3 "/home/svu/e0407728/My_FYP/TCGConv/main.py" --dataset_name "R" --percentage 1 --type_ED "sub" --num_groups 60 --root "/home/svu/e0407728/My_FYP/TCGConv/" --num_version $PBS_JOBID --seed $PBS_JOBID --alpha 0.01935191113 --gamma 3 --lr 4.19e-5 --weight_decay 4.47e-6 --hidden_chnl 128 --dropout 0.5 --num_layers 2 --model_type "TCGConv"
 EOF
