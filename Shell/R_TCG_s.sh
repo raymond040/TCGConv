2378bbcfa20461da67feef1f5ccd4c75d2380b35
raymond040 <julianto.raymondfendy@gmail.com>
2023-01-15 00:51:21 +0800

    shell to use cpus and change alpha to suit positive class rate

--- Shell/R_TCG_s.sh
+++ Shell/R_TCG_s.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 #PBS -P Project
 #PBS -j oe
-#PBS -q volta_gpu
-#PBS -l select=1:ncpus=8:mem=80gb
-#PBS -l walltime=12:00:00
+#PBS -q parallel24
+#PBS -l select=4:ncpus=24:mem=160gb
+#PBS -l walltime=720:00:00
+#PBS -N R_TCG_s
+
 
 cd #PBS_o_WORKDIR;
 np=$(cat ${PBS_NODEFILE} | wc -l);
@@ -11,5 +13,5 @@ np=$(cat ${PBS_NODEFILE} | wc -l);
 image="/home/svu/e0407728/SIF/edge-hpc_v0.1.sif"
 singularity exec -e $image bash << EOF > $PBS_JOBID.$PBS_JOBNAME.out 2> $PBS_JOBID.$PBS_JOBNAME.err
 
-python3 "/home/svu/e0407728/My_FYP/TCGConv/main.py" --dataset_name "R" --percentage 1 --type_ED "sub" --num_groups 30 --root "/home/svu/e0407728/My_FYP/TCGConv/" --num_version $PBS_JOBID --seed $PBS_JOBID --alpha 0.01935191113 --gamma 3 --lr 4.19e-5 --weight_decay 4.47e-6 --hidden_chnl 128 --dropout 0.5 --num_layers 2 --model_type "TCGConv_sum"
+python3 "/home/svu/e0407728/My_FYP/TCGConv/main.py" --dataset_name "R" --percentage 1 --type_ED "sub" --num_groups 30 --root "/home/svu/e0407728/My_FYP/TCGConv/" --num_version $PBS_JOBID --seed $PBS_JOBID --alpha 1.935e-3 --gamma 3 --lr 4.19e-5 --weight_decay 4.47e-6 --hidden_chnl 128 --dropout 0.5 --num_layers 2 --model_type "TCGConv_sum"
 EOF
--- Shell/W_ATN.sh
+++ Shell/W_ATN.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #PBS -P Project
 #PBS -j oe
-#PBS -q volta_gpu
-#PBS -l select=1:ncpus=8:mem=80gb
-#PBS -l walltime=12:00:00
+#PBS -q parallel24
+#PBS -l select=4:ncpus=24:mem=160gb
+#PBS -l walltime=720:00:00
+#PBS -N W_ATN
 
 cd #PBS_o_WORKDIR;
 np=$(cat ${PBS_NODEFILE} | wc -l);
@@ -11,5 +12,5 @@ np=$(cat ${PBS_NODEFILE} | wc -l);
 image="/home/svu/e0407728/SIF/edge-hpc_v0.1.sif"
 singularity exec -e $image bash << EOF > $PBS_JOBID.$PBS_JOBNAME.out 2> $PBS_JOBID.$PBS_JOBNAME.err 
 
-python3 "/home/svu/e0407728/My_FYP/TCGConv/main.py" --dataset_name "W" --percentage 1 --type_ED "sub" --num_groups 15 --root "/home/svu/e0407728/My_FYP/TCGConv/" --num_version $PBS_JOBID --seed $PBS_JOBID --alpha 0.00387038222 --gamma 3 --lr 4.19e-5 --weight_decay 4.47e-6 --hidden_chnl 128 --dropout 0.5 --num_layers 2 --model_type "ATN"
+python3 "/home/svu/e0407728/My_FYP/TCGConv/main.py" --dataset_name "W" --percentage 1 --type_ED "sub" --num_groups 15 --root "/home/svu/e0407728/My_FYP/TCGConv/" --num_version $PBS_JOBID --seed $PBS_JOBID --alpha 1.965e-03  --gamma 3 --lr 4.19e-5 --weight_decay 4.47e-6 --hidden_chnl 128 --dropout 0.5 --num_layers 2 --model_type "ATN"
 EOF
